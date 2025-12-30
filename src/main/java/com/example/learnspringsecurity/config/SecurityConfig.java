165a538ebbfa982242ddae3e9d56a7ff10fb33a7
Soyebuzaman Naim <soyebuzamannaim@gmail.com>
2025-12-30 10:47:09 +0600

    login logout and remember me set up done

--- src/main/java/com/example/learnspringsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/learnspringsecurity/config/SecurityConfig.java
@@ -18,10 +18,23 @@ public class SecurityConfig {
                 .requestMatchers("/dashboard", "/").authenticated()
                 .requestMatchers("/admin", "/admin/*", "/admin/**").denyAll()
                 .requestMatchers("/*.css").permitAll()
-                .requestMatchers("/about","/logout").permitAll()
-        ).formLogin(form->form
+                .requestMatchers("/about", "/logout").permitAll()
+        ).formLogin(form -> form
                 .loginPage("/login")
                 .usernameParameter("mobile")
+                .defaultSuccessUrl("/dashboard",true)//for smart routing use false
+                //true will force user to the dashboard anyway
+                .failureUrl("/login?error")
+        ).logout(logout->logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID","moneRakhio1Mash")
+        ).rememberMe(form -> form
+                .rememberMeParameter("remember-me")
+                .rememberMeCookieName("moneRakhio1Mash")//changed remember me cookie name
+                .tokenValiditySeconds(2592000)
         )
 
 
